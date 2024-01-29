423e43cc817745885b03e0013314e4c016abd937
Ramón Menor <ramonmenor@gmail.com>
2024-01-29 22:51:19 +0100

    Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://ramonmenor.es',
-
-	integrations: [mdx(), sitemap()],
+	integrations: [sitemap(), mdx()],
 });
