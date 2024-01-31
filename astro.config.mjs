9eac1b2d8660cbb8eaa912559587c0a06f2d87af
Ramón Menor <ramonmenor@gmail.com>
2024-01-31 22:35:22 +0100

    Actualización

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import auth from "auth-astro";
+//import auth from "auth-astro";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ramonmenor.es',
-  integrations: [sitemap(), mdx(), auth()],
+  integrations: [
+    sitemap(), 
+    mdx(), 
+  //  auth()
+  ],
   output: "server",
   adapter: vercel()
 });
