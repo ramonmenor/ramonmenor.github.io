89938d9827fd259c154e725dd46922defb753ed1
Ramón Menor <ramonmenor@gmail.com>
2024-01-31 22:55:07 +0100

    Quitar auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 //import auth from "auth-astro";
 
-import vercel from "@astrojs/vercel/serverless";
+//import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +14,5 @@ export default defineConfig({
   //  auth()
   ],
   output: "server",
-  adapter: vercel()
+  //adapter: vercel()
 });
