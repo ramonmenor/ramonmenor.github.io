16f5fd27cfb061373f18f46e17cf94f4bfee0d69
Ramón Menor <ramonmenor@gmail.com>
2024-01-31 23:05:26 +0100

    Quitar login

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-//import auth from "auth-astro";
-
-//import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,8 +8,6 @@ export default defineConfig({
   integrations: [
     sitemap(), 
     mdx(), 
-  //  auth()
   ],
-  output: "server",
-  //adapter: vercel()
+  output: "server"
 });
