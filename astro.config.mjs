da6e53c1c877777281e61bd425e36f341ec8b55e
Ramón Menor <ramonmenor@gmail.com>
2024-02-28 21:36:19 +0100

    Añado tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ramonmenor.es',
-  integrations: [
-    sitemap(), 
-    mdx(), 
-  ]
+  integrations: [sitemap(), mdx(), tailwind()]
 });
